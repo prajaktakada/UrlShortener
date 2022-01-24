4ea8f8569d002ab7c717c624bb84442b41186212
prajakta Kadam <93575988+prajaktakada@users.noreply.github.com>
2022-01-24 08:46:42 +0530

    Update urlController.js

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -37,34 +37,13 @@ const createUrl = async function(req, res) {
             if (!/(.com|.org|.co.in|.in|.co|.us)/.test(longUrl)) 
         return res.status(400).send({ status: false, message: `This is not a valid Url` });
     }
-    if (
-        (longUrl.includes("https://") &&
-            longUrl.match(/https:\/\//g).length !== 1) ||
-        (longUrl.includes("http://") &&
-            longUrl.match(/http:\/\//g).length !== 1) ||
-        (longUrl.includes("ftp://") && longUrl.match(/ftp:\/\//g).length !== 1)
-    ) {
+    if ((longUrl.includes("https://") && longUrl.match(/https:\/\//g).length !== 1) ||
+        (longUrl.includes("http://") &&longUrl.match(/http:\/\//g).length !== 1) ||
+        (longUrl.includes("ftp://") && longUrl.match(/ftp:\/\//g).length !== 1)) {
         return res.status(400).send({ status: false, msg: "Url is not valid" });
     }
-  
-    if (
-        longUrl.includes("w") &&
-        (longUrl.indexOf("w") === 6 ||
-            longUrl.indexOf("w") === 7 ||
-            longUrl.indexOf("w") === 8)
-    ) {
-        let arr = [];
-        let i = longUrl.indexOf("w");
-        while (longUrl[i] == "w") {
-            if (longUrl[i] === "w") {
-                arr.push(longUrl[i]);
-            }
-            i++;
-        }
-        if (!(arr.length === 3)) {
-            return res.status(400).send({ status: false, msg: "Url is not valid" });
-        }
-    }//validation end
+
+    //validation end
 
         if (validUrl.isUri(longUrl)) { //used  valid url package
             const urlToken = shortid.generate();
